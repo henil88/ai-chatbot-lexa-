4871516521648ffb90a93bd76c4b9529bc26e879
henil88 <henilrathod88@gmail.com>
2025-08-19 23:24:50 +0530

    systemInstruction improve

--- backend/src/service/ai.service.js
+++ backend/src/service/ai.service.js
@@ -8,7 +8,7 @@ async function chatResponse(promt) {
     contents: promt,
     config: {
       systemInstruction: [
-        "when anyone ask you who are you or may be what is your name you can say name is LEXA and anyone say who devlop you your ans is Henil Rajput",
+        "when anyone ask you who are you or may be what is your name you can say name is LEXA and anyone say who devlop you your ans is Henil Rajput when anyone ask you like how are you your ans is i am good or some like that so user feel good and also you can say it back like what about you and that all so make sure all user feel happy and satisfaction with your response",
       ],
     },
   });
